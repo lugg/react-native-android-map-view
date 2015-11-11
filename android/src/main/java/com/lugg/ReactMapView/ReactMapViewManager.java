deefa94be1dd81927caf29f9e68c7c00322d5616
Eric Kreutzer <eric@lu.gg>
2015-11-10 18:00:44 -0800

    Handle MapView#getMap being null

--- android/src/main/java/com/lugg/ReactMapView/ReactMapViewManager.java
+++ android/src/main/java/com/lugg/ReactMapView/ReactMapViewManager.java
@@ -9,8 +9,9 @@ import android.os.SystemClock;
 
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
@@ -29,6 +30,7 @@ import com.lugg.ReactMapView.events.RegionChangeEvent;
 import com.lugg.ReactMapView.events.UserLocationUpdateEvent;
 
 public class ReactMapViewManager extends SimpleViewManager<MapView> {
+
   public static final String REACT_CLASS = "MapViewAndroid";
 
   private static final String PROP_SHOWS_USER_LOCATION = "showsUserLocation";
@@ -36,12 +38,13 @@ public class ReactMapViewManager extends SimpleViewManager<MapView> {
   private static final String PROP_SCROLL_ENABLED = "scrollEnabled";
   private static final String PROP_ZOOM_ENABLED = "zoomEnabled";
 
+  private static final String LOG_TAG = "ReactNative";
+
   // the desired zoom level, in the range of 2.0 to 21.0. Values below this range are set to 2.0, and values above it are set to 21.0.
   // Increase the value to zoom in. Not all areas have tiles at the largest zoom levels.
   private static final float DEFAULT_ZOOM_LEVEL = 12.0f;
 
-  private static final float DEFAULT_ANIMATION_DURATION = 100;
-
+  private MapView mMapView = null;
   private boolean mFollowsUserLocation = true;
   private boolean mMapLoaded = false;
 
@@ -52,36 +55,52 @@ public class ReactMapViewManager extends SimpleViewManager<MapView> {
 
   @Override
   protected MapView createViewInstance(ThemedReactContext context) {
-    MapView view = new MapView(context);
+    mMapView = new MapView(context);
     view.onCreate(null);
     view.onResume();
+
+    if (view.getMap(); == null) {
+      logError("GoogleMap is null. It's likely Google Play Services is not installed.");
+    }
 
     return view;
   }
 
   @ReactProp(name = PROP_SHOWS_USER_LOCATION, defaultBoolean = true)
-  public void setShowsUserLocation(MapView view, boolean showsUserLocation) {
+  public void setShowsUserLocation(MapView view, final boolean showsUserLocation) {
     GoogleMap map = view.getMap();
-    map.getUiSettings().setMyLocationButtonEnabled(showsUserLocation);
-    map.setMyLocationEnabled(showsUserLocation);
+
+    if (map != null) {
+      map.setMyLocationEnabled(true);
+      map.getUiSettings().setMyLocationButtonEnabled(true);
+    }
   }
 
   @ReactProp(name = PROP_ROTATE_ENABLED, defaultBoolean = false)
-  public void setRotateEnabled(MapView view, boolean rotateEnabled) {
+  public void setRotateEnabled(MapView view, final boolean rotateEnabled) {
     GoogleMap map = view.getMap();
-    map.getUiSettings().setRotateGesturesEnabled(rotateEnabled);
+
+    if (map != null) {
+      map.getUiSettings().setRotateGesturesEnabled(rotateEnabled);
+    }
   }
 
   @ReactProp(name = PROP_SCROLL_ENABLED, defaultBoolean = true)
-  public void setScrollEnabled(MapView view, boolean scrollEnabled) {
+  public void setScrollEnabled(MapView view, final boolean scrollEnabled) {
     GoogleMap map = view.getMap();
-    map.getUiSettings().setScrollGesturesEnabled(scrollEnabled);
+
+    if (map != null) {
+      map.getUiSettings().setScrollGesturesEnabled(scrollEnabled);
+    }
   }
 
   @ReactProp(name = PROP_ZOOM_ENABLED, defaultBoolean = true)
-  public void setZoomEnabled(MapView view, boolean zoomEnabled) {
+  public void setZoomEnabled(MapView view, final boolean zoomEnabled) {
     GoogleMap map = view.getMap();
-    map.getUiSettings().setZoomGesturesEnabled(zoomEnabled);
+
+    if (map != null) {
+      map.getUiSettings().setZoomGesturesEnabled(zoomEnabled);
+    }
   }
 
   @Override
@@ -95,7 +114,10 @@ public class ReactMapViewManager extends SimpleViewManager<MapView> {
   @Override
   protected void addEventEmitters(ThemedReactContext reactContext, final MapView view) {
     final GoogleMap map = view.getMap();
+    if (map == null) return;
+
     final EventDispatcher eventDispatcher = reactContext.getNativeModule(UIManagerModule.class).getEventDispatcher();
+
 
     map.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
       @Override
@@ -108,10 +130,10 @@ public class ReactMapViewManager extends SimpleViewManager<MapView> {
       }
     });
 
-    // https://github.com/facebook/react-native/blob/master/React/Views/RCTMapManager.m#L115
     map.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
       @Override
       public void onMyLocationChange(Location location) {
+        // https://github.com/facebook/react-native/blob/master/React/Views/RCTMapManager.m#L115
         if (mFollowsUserLocation) {
           map.moveCamera(CameraUpdateFactory.newLatLngZoom(
             new LatLng(location.getLatitude(), location.getLongitude()),
@@ -134,5 +156,9 @@ public class ReactMapViewManager extends SimpleViewManager<MapView> {
         mMapLoaded = true;
       }
     });
+  }
+
+  private void logError(String message) {
+    Log.e(LOG_TAG, message);
   }
 }
